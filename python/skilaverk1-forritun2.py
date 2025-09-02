0120c8265c0056e06c75fa4eaabd8de6102e241d
Theodór Leví Halldórsson <theodorlevi@protonmail.com>
2025-09-02 20:24:26 +0000

    heima verk sv1-f2

---
+++ python/skilaverk1-forritun2.py
@@ -0,0 +1,68 @@
+#!/bin/python
+
+import random
+
+def main():
+    val = int(input("vinsamlegast veldu lið 1, 6(hætta): "))
+
+
+    match val:
+        case 1:
+            listi()
+
+        case 6:
+            return 0
+    return None
+
+
+def listi():
+    tölulisti = []
+    for i in range(60):
+        tölulisti.append(random.randint(10, 99))
+
+    print("þú getur:\n1. prentað út listan 10 tölur í línu\n2. prentað út hversu oft talan 15 kemur\n3. prentað út meðaltal talnana\n4. prentað listan í öfugri röð 20 í línu\n5. prenta hversu margar tölur eru á bilinu 50 - 99")
+    val = int(input("hvað villtu gera: "))
+
+    match val:
+        case 1:
+            for i in range(len(tölulisti)):
+                print(tölulisti[i] + " ", end='')
+                if i % 10 == 0:
+                    print("\n", end='')
+            print("\n")
+
+        case 2:
+            total15 = 0
+            for tala in tölulisti:
+                if tala == 15:
+                    total15 += 1
+
+            print(total15)
+
+        case 3:
+            total = 0
+            for tala in tölulisti:
+                total += tala
+
+            print(total / 60)
+
+        case 4:
+            tölulisti.reverse()
+
+            for i in range(60):
+                print(str(tölulisti[i]) + " ", end='')
+                if i % 20 == 0:
+                    print("\n", end='')
+            print("\n")
+        case 5:
+            total = 0
+            for tala in tölulisti:
+                if tala >= 50:
+                    total += 1
+            print(total)
+
+
+
+while True:
+    if main() == 0:
+        break
